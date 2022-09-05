0a6e3ef0109ffb30149d481f049f26195142bea0
Денис <denis070619870@rambler.ru>
2022-09-05 11:37:47 +0300

    Example001_ShowNumbersNtoN

---
+++ Example001_ShowNumbersNtoN/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+
+void ShowNumber(int n, int m)
+{
+    if (n >= m)
+    {
+        Console.WriteLine($"{n} "); ShowNumber(n - 1, m);        
+    }
+}
+
+Console.WriteLine("Введите число от которого нужно показать отсчет: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Показать обратный отсчет от: {n} ");
+int m = 1;
+ShowNumber(n, m);
+Console.WriteLine();
