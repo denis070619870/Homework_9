18ac9739d23e4332baa3e68c2de379c0d0d85c02
Денис <denis070619870@rambler.ru>
2022-09-05 12:13:40 +0300

    Example002_ShowNumbersMtoN

---
+++ Example002_ShowNumbersMtoN/Program.cs
@@ -0,0 +1,17 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+
+void Number(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m); Number(m + 1, n);
+    }
+}
+
+Console.WriteLine("Введите число от которого будт показаны все числа: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число до которого будт показаны все числа: ");
+int m = int.Parse(Console.ReadLine());
+
+Number(m, n);
+Console.WriteLine();
