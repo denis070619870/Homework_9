82cfb3eb3967fcda98266b6286e1063ca1a4fe03
Денис <denis070619870@rambler.ru>
2022-09-06 16:58:53 +0300

    Example003_SumMtoN

---
+++ Example003_SumMtoN/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти сумму элементов от M до N, N и M заданы
+
+int SumNumber(int m, int n)
+{
+    if (n == m) return m;
+    else return n + SumNumber (n - 1, m);
+}
+
+Console.WriteLine("Введите число от которого будт показаны все числа: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число до которого будт показаны все числа: ");
+int n = int.Parse(Console.ReadLine());
+
+SumNumber(m, n);
+Console.WriteLine(SumNumber(m, n));
