38f1cae215f4818c7d836c38e0cc35f87b9c8093
Денис <denis070619870@rambler.ru>
2022-09-06 16:00:03 +0300

    Example002_ShowNumbersMtoN

--- Example002_ShowNumbersMtoN/Program.cs
+++ Example002_ShowNumbersMtoN/Program.cs
@@ -4,14 +4,14 @@ void Number(int m, int n)
 {
     if (m <= n)
     {
-        Console.Write(m); Number(m + 1, n);
+        Console.Write($"{m}, "); Number(m + 1, n);
     }
 }
 
 Console.WriteLine("Введите число от которого будт показаны все числа: ");
-int n = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число до которого будт показаны все числа: ");
 int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число до которого будт показаны все числа: ");
+int n = int.Parse(Console.ReadLine());
 
 Number(m, n);
 Console.WriteLine();
