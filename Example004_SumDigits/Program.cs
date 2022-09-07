f5010573e4feadf1a302c583c66c4c61bc53b419
Денис <denis070619870@rambler.ru>
2022-09-07 20:31:42 +0300

    Example004_SumDigits

---
+++ Example004_SumDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму цифр числа
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SplitNumber(int number)
+{
+    if(number == 0) return 0;
+    else return number % 10 + SplitNumber(number / 10);
+}
+
+
+int number = InputNumber("Введите число, чтобы посчитать сумму цифр: ");
+Console.WriteLine(SplitNumber(number));
