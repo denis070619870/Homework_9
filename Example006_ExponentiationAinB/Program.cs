827509b0db31b15568b892bb1cd262e5a2caa8cb
Денис <denis070619870@rambler.ru>
2022-09-07 21:45:47 +0300

    Example006_ExponentiationAinB

---
+++ Example006_ExponentiationAinB/Program.cs
@@ -0,0 +1,15 @@
+﻿//Написать программу возведения числа А в целую стень B
+
+int Exponentiation(int m, int n)
+{
+    if (n == 1) return m;
+    else return m * Exponentiation(m, n - 1);
+}
+
+Console.WriteLine("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int n = int.Parse(Console.ReadLine());
+
+Exponentiation(m, n);
+Console.WriteLine(Exponentiation(m, n));
