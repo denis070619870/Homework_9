7ca185d7e4e3a1543c61fd42488ccd0fb495fc6e
Денис <denis070619870@rambler.ru>
2022-09-07 21:16:12 +0300

    Example005_AckermanFunctions

---
+++ Example005_AckermanFunctions/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу вычисления функции Аккермана
+
+static int FunctionAcker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return FunctionAcker(m - 1, 1);
+    }
+    else
+    {
+        return FunctionAcker(m - 1, FunctionAcker(m, n - 1));
+    }
+}
+
+Console.WriteLine("Введите 1-e числоа: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2-e числоа: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write(FunctionAcker(m, n));
